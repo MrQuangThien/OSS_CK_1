9bb09ba8abd111c92861df66f1ad04b75450737d
Phạm Nguyễn Minh Thống <1250080188@sv.hcmunre.edu.vn>
2026-05-01 21:13:28 +0700

    fix import model

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import SanPham, LoaiHang
+from .models import SanPham, LoaiHang, NhaCungCap, ChiTietPhieuNhap, PhieuNhap
 
 admin.site.register(SanPham)
 admin.site.register(LoaiHang)
